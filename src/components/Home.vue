65e48ae3c632ff6ea80ce7209d0bb588b60b1702
Octavio Santiago <santiagolepe@gmail.com>
2017-12-08 17:52:23 -0600

    init clean Vue app

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
