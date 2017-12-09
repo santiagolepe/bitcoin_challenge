c36ac1cc02af573870ee94db6877346c1783776a
Octavio Santiago <santiagolepe@gmail.com>
2017-12-09 01:54:24 -0600

    get last 10 updated coins

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,41 @@ import axios from 'axios'
 
 Vue.use(Vuex)
 
+let coins = []
+for (let x = 0; x < 15; x++) {
+  coins.push({
+    BTC: {USD: 0, EUR: 0},
+    ETH: {USD: 0, EUR: 0},
+    date: ''
+  })
+}
+
+let setDate = function (e) {
+  let date = new Date(e)
+  let current = new Date()
+  current.setHours(date.getHours())
+  current.setMinutes(date.getMinutes())
+  return current.toString()
+}
+
 // State
 const state = {
-  coins: []
+  coins: coins
 }
 
 // Mutations - they can change the state values, its must be Synchronous
 const mutations = {
   setCoins (state, { data }) {
-    state.coins.push(data)
+    data.date = new Date()
+    state.coins.splice(0, 1, data)
+  },
+// {"BTC":{"USD":15819.66,"EUR":13512.38},"ETH":{"USD":466.34,"EUR":387.85}}
+  setLast10 (state, { data, query }) {
+    data.Data.forEach((e, i) => {
+      state.coins[i][query.from][query.to] = e.open
+      state.coins[i].date = new Date(setDate(e.time))
+    })
+    state.coins.shift()
   }
 }
 
@@ -24,6 +50,15 @@ const actions = {
       commit('setCoins', { data })
     } catch (err) {
       console.log('Error store getCoins', err)
+    }
+  },
+
+  async getLast10 ({ commit }, query) {
+    try {
+      const { data } = await axios.get(`https://min-api.cryptocompare.com/data/histominute?fsym=${query.from}&tsym=${query.to}&limit=10`)
+      commit('setLast10', { data, query })
+    } catch (err) {
+      console.log(`Error getLast10 ${err}`)
     }
   }
 }
