751c9ae1633a512c548218d2cf9b8a0ab919466d
Octavio Santiago <santiagolepe@gmail.com>
2017-12-09 14:14:11 -0600

    Testing Scenario: BTC to USD/EUR

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -30,7 +30,27 @@ module.exports = {
 
     browser.expect.element('#ETH_current')
     .text.to.not.contain('1 ETH = 0 USD').after(10000);
+  },
+  
 
-    browser.end()
+  // Given I'm a visitor
+  // When I enter into the site
+  // And I select BTC as cryptocurrency in the calculator
+  // And I enter 200 in the input
+  // Then I should see the equivalent in USD
+  // And If I change the currency to EUR
+  // Then The calculator should update the equivalent in EUR
+
+  '2. BTC to USD/EUR': function (browser) {
+    browser.setValue('#bitcoin_sel', 'BTC').pause(5000);
+    browser.setValue('#bitcoin_input', 200);
+    browser.setValue('#currency_sel', 'USD').pause(5000);
+    browser.expect.element('#currency_input').value.to.not.equal(0)
+    
+    browser.getValue('#currency_input', function(result){
+      browser.setValue('#currency_sel', 'EUR').pause(5000)
+      browser.expect.element('#currency_input').value.to.not.equal(result.value)
+    });
   }
+
 }
