1470572946a134f8fe47ea5234cd288533830081
Octavio Santiago <santiagolepe@gmail.com>
2017-12-09 14:45:40 -0600

    Testing Scenario: ETH to USD/EUR

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -31,7 +31,6 @@ module.exports = {
     browser.expect.element('#ETH_current')
     .text.to.not.contain('1 ETH = 0 USD').after(10000);
   },
-  
 
   // Given I'm a visitor
   // When I enter into the site
@@ -51,6 +50,28 @@ module.exports = {
       browser.setValue('#currency_sel', 'EUR').pause(5000)
       browser.expect.element('#currency_input').value.to.not.equal(result.value)
     });
-  }
+  },
 
+  // Scenario: ETH to USD/EUR
+  // Given I'm a visitor
+  // When I enter into the site
+  // And I select ETH as cryptocurrency in the calculator
+  // And I enter 200 in the input
+  // Then I should see the equivalent in USD
+  // And If I change the currency to EUR
+  // Then The calculator should update the equivalent in EUR
+
+  '3. ETH to USD/EUR': function (browser) {
+    browser.clearValue('#bitcoin_input', function(res){
+      browser.setValue('#bitcoin_sel', 'ETH').pause(5000);
+      browser.setValue('#bitcoin_input', 200);
+      browser.setValue('#currency_sel', 'USD').pause(5000);
+      browser.expect.element('#currency_input').value.to.not.equal(0)
+      
+      browser.getValue('#currency_input', function(result){
+        browser.setValue('#currency_sel', 'EUR').pause(5000)
+        browser.expect.element('#currency_input').value.to.not.equal(result.value)
+      });
+    });
+  }
 }
