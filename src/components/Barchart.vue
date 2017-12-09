b721e27e34230f3086566c606890795772080792
Octavio Santiago <santiagolepe@gmail.com>
2017-12-09 01:54:57 -0600

    add Barchart component to render charts

---
+++ src/components/Barchart.vue
@@ -0,0 +1,18 @@
+<script>
+import { Bar } from 'vue-chartjs'
+export default {
+  name: 'Barchart',
+  extends: Bar,
+  props: {
+    chartData: Object
+  },
+  methods: {
+    update () {
+      this.$data._chart.update()
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, {maintainAspectRatio: false})
+  }
+}
+</script>
