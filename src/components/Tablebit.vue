97e7d939d07ba5896703a49b4279881bf48f2597
Octavio Santiago <santiagolepe@gmail.com>
2017-12-09 02:38:56 -0600

    create tablebit component for reusable table boostrap

---
+++ src/components/Tablebit.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-card bg-variant="default"
+      text-variant="black"
+      :header="header"
+      class="text-center">
+      <b-table striped hover :items="items"></b-table>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tablebit',
+  data: () => ({
+    items: null
+  }),
+  props: {
+    header: String,
+    coins: Array,
+    type: String
+  },
+  methods: {
+    update () {
+      this.items = []
+      this.coins.forEach(i => {
+        this.items.push({
+          Time: i.date,
+          USD: i[this.type].USD,
+          EUR: i[this.type].EUR
+        })
+      })
+    }
+  }
+}
+</script>
