9ab6ab0fcacf8cf56a26f8b42372e885be5c8516
Octavio Santiago <santiagolepe@gmail.com>
2017-12-09 14:47:00 -0600

    Testing Scenario: ETH to USD/EUR

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -9,10 +9,10 @@
 
       <div class="form-group row">
         <div class="col-sm-6">
-          <input type="number" class="form-control" v-model.number="bitcoin" v-on:change="calculate()">
+          <input id="bitcoin_input" type="number" class="form-control" v-model.number="bitcoin" v-on:change="calculate()">
         </div>                
         <div class="col-sm-6">
-          <select  class="form-control" v-model="bitcoin_sel" v-on:change="calculate()">
+          <select id="bitcoin_sel" class="form-control" v-model="bitcoin_sel" v-on:change="calculate()">
             <option value="BTC">BTC</option>
             <option value="ETH">ETH</option>
           </select>
@@ -21,10 +21,10 @@
 
       <div class="form-group row">                
         <div class="col-sm-6">
-          <input type="number" class="form-control" v-model.number="currency">
+          <input id="currency_input" type="number" class="form-control" v-model.number="currency">
         </div>                
         <div class="col-sm-6">
-          <select class="form-control" v-model="currency_sel" v-on:change="calculate()">
+          <select id="currency_sel" class="form-control" v-model="currency_sel" v-on:change="calculate()">
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
           </select>
