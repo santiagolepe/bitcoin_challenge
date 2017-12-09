8a2053e29673919faf1f8298eaf9df0eebf235dd
Octavio Santiago <santiagolepe@gmail.com>
2017-12-08 20:12:46 -0600

    navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">CCD</b-navbar-brand>
+    </b-collapse>
+  </b-navbar> 
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
